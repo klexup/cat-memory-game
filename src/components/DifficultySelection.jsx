d8bde340a5cbd58ff96c29ff3d98fc747c697498
Klexup <jameshensley37@gmail.com>
2023-10-22 10:15:46 -0600

    added rules and compressed images

--- src/components/DifficultySelection.jsx
+++ src/components/DifficultySelection.jsx
@@ -7,6 +7,12 @@ export default function DifficultySelection({ setDifficulty, resetGame }) {
   }
   return (
     <>
+      <div className="rules-container">
+        <p>- Click on a cat image to earn a point.</p>
+        <p>- After each click, the images shuffle.</p>
+        <p>- Clicking the same cat twice resets your score.</p>
+        <p>- Aim to beat your highest score!</p>
+      </div>
       <div className="difficulty-container">
         <button
           onClick={() => {
